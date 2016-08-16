a1321ab360b589a65d333d3518528f285a39abd0
baardl <bard.lind@gmail.com>
2016-08-16 08:46:58 +0200

    Cleanup

--- www/aws-config.js
+++ www/aws-config.js
@@ -1,6 +1,5 @@
 evothings.aws = evothings.aws || {}
 
-// TODO: Fill in your AWS information here.
 evothings.aws.config = {
 	accessKeyId: 'AKIAI3AQYARUN5U76XDA',
 	secretAccessKey: '/2QtUrzro8Styzh+xuAkEpJWlJqqvq+DBSYR3owB',
