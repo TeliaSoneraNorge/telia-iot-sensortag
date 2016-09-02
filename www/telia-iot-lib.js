e18278889582294b57fcd47281616f23f8350da4
baardl <bard.lind@gmail.com>
2016-09-02 15:10:56 +0200

    #2 Write to Telia IoT ok.

--- www/telia-iot-lib.js
+++ www/telia-iot-lib.js
@@ -6,63 +6,37 @@ evothings.telia = evothings.telia || {}
 evothings.telia.iot  = evothings.telia.iot  || {}
 
 
-/**
- * Initialize AWS Lambda.
- * @param config Object with configuration parameters,
- * see file aws-config.js
- */
-evothings.telia.iot.initialize = function(config)
-{
-    console.log("Initialize. Try to access " + config.healthEndpoint);
+
+evothings.telia.iot.initialize = function(config){
+    evothings.telia.iot.baseDeviceUrl = config.devicedataEnpoint;
+
+}
+
+evothings.telia.iot.update = function(deviceId,payload,success, error){
+    console.log("Try to access " + evothings.telia.iot.baseDeviceUrl);
 
     var xhttp = new XMLHttpRequest();
 
-    //xhttp.open("GET", config.healthEndpoint, true);
-    xhttp.open("GET",config.healthEndpoint + "1234", true);
+    xhttp.open("POST", evothings.telia.iot.baseDeviceUrl + "/" + deviceId, payload);
+    //xhttp.open("GET",config.healthEndpoint + "1234", true);
     xhttp.onreadystatechange = function() {
-        if (this.readyState == 4 && this.status == 200) {
-            console.log("Response: " +this.responseText);
+        if (this.readyState == 4 ) {
+            if (this.status == 200 || this.status == 201) {
+                console.log("Response: " + this.responseText);
+                success("Updated device: " + deviceId + ", with data: " + payload);
+            }
         } else {
-            console.log("State: " + this.readyState + ", status: " + this.status + ", response: " + this.responseText);
+            //console.log("State: " + this.readyState + ", status: " + this.status + ", response: " + this.responseText);
         }
     };
-    xhttp.onerror = function(){console.log("error: " + xhttp.status + ", errortext: " + xhttp.statusText)}
 
-    /*request.onreadystatechange = function() {//Call a function when the state changes.
-        if (request.readyState == 4) {
-            if (request.status == 200 || request.status == 0) {
-                var tweets = JSON.parse(request.responseText);
-                var data = "<table cellspacing='0'>";
-                var tableClass;
-                for (i = 0; i < tweets.results.length; i++) {
-                    if (i % 2 == 0) {
-                        tableClass = 'tweetOdd';
-                    }
-                    else {
-                        tableClass = 'tweetEven';
-                    }
-                    data += "<tr style='border: 1px solid black'>";
-                    data += "<td class='" + tableClass + "'>";
-                    data += "<img src='" + tweets.results[i].profile_image_url + "'/>";
-                    data += "</td>";
-                    data += "<td class='" + tableClass + "'>";
-                    data += "<b>" + tweets.results[i].from_user + "</b><br/>";
-                    data += tweets.results[i].text + "<br/>";
-                    data += tweets.results[i].created_at;
-                    data += "</td>";
-                    data += "</tr>";
-                }
-                data += "</table>";
-                var twitter = document.getElementById("latestTweets");
-                twitter.innerHTML = data;
-            }
-        }
+    xhttp.onerror = function(){
+        console.log("error: " + xhttp.status + ", errortext: " + xhttp.statusText);
+        error("Failed to update to " + evothings.telia.iot.baseDeviceUrl + ". Reason: " + xhttp.statusText);
+
     }
-    console.log("asking for tweets");
-    */
+
+
     xhttp.send();
-
-
-
 }
 
